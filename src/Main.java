aff0aa2e62f56051102e8d21932dff2a06e203b2
woodcutter33 <agafandrei@gmail.com>
2023-01-19 11:01:48 +0300

    рефакторинг

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,32 @@ import java.util.Stack;
 
 public class Main {
 
-    public static int countBracket(String s) {
-        int count = 0;
-        Stack<Integer> stack = new Stack<>();
-        stack.push(-1);
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                stack.push(i);
-            } else {
-                stack.pop();
-                if (stack.empty()) {
-                    stack.push(i);
-                } else {
-                    count = Math.max(count, i - stack.peek());
-                }
-            }
-        }
-
-        return count;
-    }
-
-    public static void main(String[] args) {
-        String s = ")";
+    public static void countBracket(String s) {
         if (!s.contains("(") || !s.contains(")") || s.isEmpty()) {
             System.out.println("Условия не выполнены");
         } else {
-            int rez = countBracket(s);
-            String bracket = "()".repeat(rez);
-            System.out.print(rez + " - " + bracket);
+            int count = 0;
+            Stack<Integer> stack = new Stack<>();
+            stack.push(-1);
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '(') {
+                    stack.push(i);
+                } else {
+                    stack.pop();
+                    if (stack.empty()) {
+                        stack.push(i);
+                    } else {
+                        count = Math.max(count, i - stack.peek());
+                    }
+                }
+            }
+            System.out.print(count + " - " + "()".repeat(count / 2));
         }
     }
+
+    public static void main(String[] args) {
+        String s = ")()()";
+        countBracket(s);
+    }
 }
+
